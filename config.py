38dc7086fa9707cf2e3bf0995faaf6e16416bbaa
Phr3d13 <phr3d13@gmail.com>
2024-05-03 20:56:41 -0400

    Thing

---
+++ config.py
@@ -0,0 +1,10 @@
+HOST = "irc.twitch.tv"              # This is Twitchs IRC server
+PORT = 6667                         # Twitchs IRC server listens on port 6767
+NICK = "phr3d13_bot"            # Twitch username your using for your bot
+PASS = "oauth:9fr60wbt3acrs07fm4q5is56heq68m" # your Twitch OAuth token
+CHAN = "#phr3d13"                   # the channel you want the bot to join.
+RATE = (20/30) # messages per seccond
+BAN_PAT = [
+    r"swear",
+    r"some_pattern"
+]
